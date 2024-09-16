ae9fde579337278e3d59103604b12870ff190232
vfernandes617 <167803670+vfernandes617@users.noreply.github.com>
2024-09-15 20:59:40 -0400

    Add files via upload

---
+++ mini_solar_sys/Assets/C# Scripts/Loadscene.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
